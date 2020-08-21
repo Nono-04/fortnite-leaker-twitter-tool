55299d31ff65ffbdb0a48fcc85aade01f288ade9
Tamino Seiler <49478749+Nono-04@users.noreply.github.com>
2020-08-21 07:19:31 +0200

    Update SETTINGS.py

--- settings/SETTINGS.py
+++ settings/SETTINGS.py
@@ -17,10 +17,10 @@ intervall = 30 # Under 20 Seconds is not allowed.
 Enter here you Twitter Tokens from https://developer.twitter.com/en/apps
 """
 TWITTER_TOKEN = {
-                "consumer_key": "3X4M8taLKe1OpJd1ZY3j18XfB",
-                "consumer_secret": "ASv1NFGP6imxcsABiFVJlky4VG8SGA7ge5jbIjoObUtaEDpZV5",
-                "access_token_key": "1054765326313947141-a6uEKxtU1EnMSsWfpwb5G27COwli8p",
-                "access_token_secret": "HpOfrsMMqQEaRFiyxiFNLfilgEMT8qLYLl9Aby1uZUOiH",
+                "consumer_key": "xxxxxxxx",
+                "consumer_secret": "xxxxxxxxxxx",
+                "access_token_key": "xxxx-x",
+                "access_token_secret": "xxx",
             }
 
 nopost = False  # A FUNCTION FOR TESTING! Leave it on False or the Bot dont work!!!!
